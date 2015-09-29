3744f14a443bbcec5c8544d55e58ae03d5244a74
Siddharth Chatrola <sichatro@microsoft.com>
2015-09-29 16:53:33 -0700

    Removing CommonAssemblyInfo so we can create seperate release for output cache and session state

--- test/OutputCacheProviderUnitTests/Properties/AssemblyInfo.cs
+++ test/OutputCacheProviderUnitTests/Properties/AssemblyInfo.cs
@@ -38,3 +38,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
+#if !NOCOMMONASSEMBLYVERSION
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+#endif
+[assembly: AssemblyTitle("Cache Providers")]
