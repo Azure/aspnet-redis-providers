6c1ba24a5cbc9cc4e51b44bee9afb7042fa4533d
Siddharth Chatrola <sichatro@microsoft.com>
2017-09-28 13:50:49 -0700

    Fixing test TestingForceReconnect

--- test/Shared/RedisServer.cs
+++ test/Shared/RedisServer.cs
@@ -39,12 +39,12 @@ namespace Microsoft.Web.Redis.FunctionalTests
 
         public RedisServer()
         {
-            Restart();
+            KillRedisServers();
+            Start();
         }
 
-        public void Restart()
+        public void Start()
         {
-            KillRedisServers();
             _server = new Process();
             _server.StartInfo.FileName = "..\\..\\..\\..\\..\\..\\packages\\redis-64.3.0.503\\tools\\redis-server.exe";
             _server.StartInfo.Arguments = "--maxmemory 20000000";
@@ -54,7 +54,7 @@ namespace Microsoft.Web.Redis.FunctionalTests
         }
 
         // Make sure that no redis-server instance is running
-        private void KillRedisServers()
+        public void KillRedisServers()
         {
             foreach (var proc in Process.GetProcessesByName("redis-server"))
             {
