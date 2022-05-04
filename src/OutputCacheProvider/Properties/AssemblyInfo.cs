c6716323af9972df8d7223c11b77076709e3698e
Stanley Small <stanleysmall@microsoft.com>
2022-05-04 13:34:49 -0400

    update assembly dates to 2022

--- src/OutputCacheProvider/Properties/AssemblyInfo.cs
+++ src/OutputCacheProvider/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OutputCacheProvider")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
