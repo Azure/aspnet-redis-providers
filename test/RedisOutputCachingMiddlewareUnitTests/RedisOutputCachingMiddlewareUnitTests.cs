fc0700949622df951e2100c5d310b4464bb0fc9c
Stanley Small <stanleysmall@microsoft.com>
2022-05-04 13:31:31 -0400

    remove ttl references in tests

--- test/RedisOutputCachingMiddlewareUnitTests/RedisOutputCachingMiddlewareUnitTests.cs
+++ test/RedisOutputCachingMiddlewareUnitTests/RedisOutputCachingMiddlewareUnitTests.cs
@@ -11,8 +11,6 @@ namespace RedisOutputCachingMiddleware.UnitTests
         {
             var reqeustDelegate = A.Fake<RequestDelegate>();
             var outputCachingMiddleware = new OutputCachingMiddleware(reqeustDelegate, "localhost");
-            var ttl = outputCachingMiddleware.GetTtl();
-            Assert.Equal(86400, ttl);
         }
 
         [Fact]
@@ -20,18 +18,6 @@ namespace RedisOutputCachingMiddleware.UnitTests
         {
             var reqeustDelegate = A.Fake<RequestDelegate>();
             var outputCachingMiddleware = new OutputCachingMiddleware(reqeustDelegate, "localhost", 123);
-            var ttl = outputCachingMiddleware.GetTtl();
-            Assert.Equal(123, ttl);
-        }
-
-        [Fact]
-        public void TestSetTtl()
-        {
-            var reqeustDelegate = A.Fake<RequestDelegate>();
-            var outputCachingMiddleware = new OutputCachingMiddleware(reqeustDelegate, "localhost");
-            outputCachingMiddleware.SetTtl(123);
-            var ttl = outputCachingMiddleware.GetTtl();
-            Assert.Equal(123, ttl);
         }
     }
 }
