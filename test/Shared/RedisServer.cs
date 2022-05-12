2615cd8b5368783312a9fdb54f29a1becbbd2d8e
Stanley Small <stanleysmall@microsoft.com>
2022-05-12 09:53:27 -0400

    add option to change redis-server.exe path

--- test/Shared/RedisServer.cs
+++ test/Shared/RedisServer.cs
@@ -46,8 +46,8 @@ namespace Microsoft.Web.Redis.FunctionalTests
         {
             KillRedisServers();
             _server = new Process();
-            string executable_path = $"{Environment.CurrentDirectory}\\..\\..\\..\\..\\..\\packages\\redis-64\\3.0.503\\tools\\redis-server.exe";
-            _server.StartInfo.FileName = executable_path;
+            string defaultExecutablePath = $"{Environment.CurrentDirectory}\\..\\..\\..\\..\\..\\packages\\redis-64\\3.0.503\\tools\\redis-server.exe";
+            _server.StartInfo.FileName = Environment.GetEnvironmentVariable("REDIS_SERVER_PATH") ?? defaultExecutablePath;
             _server.StartInfo.Arguments = "--maxmemory 20000000";
             _server.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             _server.Start();
