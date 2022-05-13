7fd7414caff7637de10df79db5f0c6d56dec6d93
Stanley Small <stanleysmall@microsoft.com>
2022-05-13 13:59:47 -0400

    Revert "update assembly dates to 2022"
    This reverts commit c6716323af9972df8d7223c11b77076709e3698e.

--- src/RedisSessionStateProvider/Properties/AssemblyInfo.cs
+++ src/RedisSessionStateProvider/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SessionStateProvider")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
