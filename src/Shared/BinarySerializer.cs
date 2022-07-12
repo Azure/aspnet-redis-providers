9b1472f76f09e4ad0071fe72b9a7d6c9761fd7ef
Stanley Small <103065678+stanleysmall-microsoft@users.noreply.github.com>
2022-07-12 01:17:49 -0400

    fix tests

--- src/Shared/BinarySerializer.cs
+++ src/Shared/BinarySerializer.cs
@@ -4,22 +4,63 @@
 //
 
 using System.IO;
+using System.Text;
+using System.Web.SessionState;
 using ProtoBuf;
 
 namespace Microsoft.Web.Redis
 {
     public class BinarySerializer : ISerializer
     {
+        private readonly string redisNull = "REDIS_NULL";
+        private readonly string initializeItem = "INITIALIZE_INSTANCE";
         public byte[] Serialize(object data)
         {
-            return Serialize(data);
+            using (var memoryStream = new MemoryStream())
+            {
+                if (data is null)
+                {
+                    data = redisNull;
+                }
+                if (data is SessionStateActions.InitializeItem)
+                {
+                    data = initializeItem;
+                }
+                Serializer.Serialize(memoryStream, data);
+                return memoryStream.ToArray();
+            }
+            
         }
 
         public object Deserialize(byte[] data)
         {
+            if (data is null)
+            {
+                return null;
+            }
+
             using (var memoryStream = new MemoryStream(data))
             {
-                return Serializer.Deserialize<byte[]>(memoryStream);
+                object retObject = Serializer.Deserialize<byte[]>(memoryStream);
+                
+                try
+                {
+                    string retString = Encoding.Default.GetString((byte[])retObject);
+                    if (retString == redisNull)
+                    {
+                        return null;
+                    }
+                    else if (retString == initializeItem)
+                    {
+                        return SessionStateActions.InitializeItem;
+                    }
+                    return retString;
+                }
+                catch
+                {
+                    return retObject;
+                }
+
             }
         }
     }
