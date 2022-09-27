aa7ea5399fcb990addc1321d11c152fcf45cfca7
Stanley Small <103065678+stanleysmall-microsoft@users.noreply.github.com>
2022-09-27 14:20:48 -0400

    rename id to sessionId

--- src/RedisSessionStateProvider/KeyGenerator.cs
+++ src/RedisSessionStateProvider/KeyGenerator.cs
@@ -14,24 +14,23 @@ namespace Microsoft.Web.Redis
         public string LockKey { get; private set; }
         public string InternalKey { get; private set; }
 
-        public KeyGenerator(string id, string applicationName)
+        public KeyGenerator(string sessionId, string applicationName)
         {
-            this.id = id;
-            DataKey = "{" + applicationName + "_" + id + "}_Data";
-            LockKey = "{" + applicationName + "_" + id + "}_Write_Lock";
-            InternalKey = "{" + applicationName + "_" + id + "}_Internal";
+            this.id = sessionId;
+            DataKey = "{" + applicationName + "_" + sessionId + "}_Data";
+            LockKey = "{" + applicationName + "_" + sessionId + "}_Write_Lock";
+            InternalKey = "{" + applicationName + "_" + sessionId + "}_Internal";
         }
 
-        public void RegenerateKeyStringIfIdModified(string id, string applicationName)
+        public void RegenerateKeyStringIfIdModified(string sessionId, string applicationName)
         {
-            if (!id.Equals(this.id))
+            if (!sessionId.Equals(this.id))
             {
-                this.id = id;
-                DataKey = "{" + applicationName + "_" + id + "}_Data";
-                LockKey = "{" + applicationName + "_" + id + "}_Write_Lock";
-                InternalKey = "{" + applicationName + "_" + id + "}_Internal";
+                this.id = sessionId;
+                DataKey = "{" + applicationName + "_" + sessionId + "}_Data";
+                LockKey = "{" + applicationName + "_" + sessionId + "}_Write_Lock";
+                InternalKey = "{" + applicationName + "_" + sessionId + "}_Internal";
             }
         }
-
     }
 }
